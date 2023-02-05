b84a94631d68a323af6de6fae522cf1c70d89485
kpratap1 <pratapk@tukwila.wednet.edu>
2023-02-05 15:32:38 -0800

    Ramsete is functional

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -12,7 +10,6 @@ import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -37,13 +34,10 @@ public class Robot extends TimedRobot {
   WPI_TalonFX intakeExternal = new WPI_TalonFX(4); // external intake motor
   DifferentialDrive drive = new DifferentialDrive(left, right);
   XboxController controller = new XboxController(0);
-  Timer timer = new Timer(); 
   ADIS16448_IMU gyro = new ADIS16448_IMU(); // RoboRIO-mounted gyroscope
+  Timer timer = new Timer(); 
   DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(new Rotation2d(0), 0,0);
-  ProfiledPIDController angleControl = new ProfiledPIDController(0.04, 0.1, 0.01, new TrapezoidProfile.Constraints(100,20));
-  double encoderTicksPerMeter = 2048*10.71/(0.0254*6*Math.PI);
-  double desiredRobotPosition = 0;
-  double desiredRobotAngle = 0;
+  double encoderTicksPerMeter = 2048*10.71/(0.0254*6*Math.PI); // theoretical 45812 ticks per meter
   // controller inputs
   double leftStickY;
   double leftStickX;
@@ -57,30 +51,17 @@ public class Robot extends TimedRobot {
   double positionBelt;
   double positionInternalIntake;
   double positionExternalIntake;
-  // match time
-  double time;
-  // gyro angle
-  double angle;
-
-  double kS = 0.13676;
-  double kV = 2.4254;
-  double kA = 0.45642;
-  double kP = 3.3778;
-  SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(kS, kV, kA);
-  
-  String trajectoryJSON = "paths/Section 1.wpilib.json";
-  Trajectory trajectory = new Trajectory();
-
-  double trackWidth = 0.69;
+  double time; // match time
+  Trajectory trajectory;
+  double trackWidth = 0.69474;  // value obtained from SysID
   DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(trackWidth);
-  double maxV = 3;
-  double maxA = 1;
-  double ramseteB = 2;
-  double ramseteZeta = 0.7;
-  RamseteController ramsete = new RamseteController(ramseteB, ramseteZeta);
+  RamseteController ramsete = new RamseteController(2, 0.7);
+  // odometry calculated robot position
+  Pose2d robotPosition;
+  double robotX;
+  double robotY;
+  double angle; // gyro angle
   
-
-
   @Override
   public void robotInit() {
     initialize();
@@ -89,9 +70,7 @@ public class Robot extends TimedRobot {
     gyro.calibrate(); // sets the gyro angle to 0 based on the current robot position
     CameraServer.startAutomaticCapture(); // starts the webcam stream
     updateVariables();
-    angleControl.setIntegratorRange(1, -1);
-    angleControl.setTolerance(1);
-    angleControl.enableContinuousInput(0, 360);
+    String trajectoryJSON = "paths/Test.wpilib.json";
     try {
       Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
       trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
@@ -105,7 +84,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    timer.reset();
+    timer.reset(); // sets the timer to 0
+    left.setNeutralMode(NeutralMode.Brake);
+    right.setNeutralMode(NeutralMode.Brake);
     updateVariables();
   }
 
@@ -113,19 +94,20 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     updateVariables();
     Trajectory.State goal = trajectory.sample(time);
-    Pose2d robotPosition = odometry.getPoseMeters();
     ChassisSpeeds chassisSpeeds = ramsete.calculate(robotPosition, goal);
     DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
     double leftSpeed = (wheelSpeeds.leftMetersPerSecond/10)*encoderTicksPerMeter;
     double rightSpeed = (wheelSpeeds.rightMetersPerSecond/10)*encoderTicksPerMeter;
-
     left.set(ControlMode.Velocity, leftSpeed);
     right.set(ControlMode.Velocity, rightSpeed);
+    drive.feed();
   }
 
   @Override
   public void teleopInit() {
-    timer.reset();
+    timer.reset(); // sets the timer to 0
+    left.setNeutralMode(NeutralMode.Brake);
+    right.setNeutralMode(NeutralMode.Brake);
     updateVariables();
   }
 
@@ -134,14 +116,17 @@ public class Robot extends TimedRobot {
     updateVariables();
     
     // sets motor speeds based on controller inputs
-    drive.arcadeDrive(-leftStickY, -leftStickX, true);
+    drive.arcadeDrive(-leftStickY, -rightStickX, true);
     belt.set(-rightTrigger);
     intakeExternal.set(leftTrigger);
     intakeInternal.set(leftTrigger);
   }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    left.setNeutralMode(NeutralMode.Coast);
+    right.setNeutralMode(NeutralMode.Coast);
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -161,31 +146,23 @@ public class Robot extends TimedRobot {
   public void initialize() {
     motorStartUp(left);
     //PID coefficients
-    left.config_kF(0, 0, 30);
-    left.config_kP(0, 1, 30);
-    left.config_kI(0, 0.005, 30);
-    left.config_kD(0, 10, 30);
-    // motion magic parameters
-    left.configMotionCruiseVelocity(20000, 30);
-    left.configMotionAcceleration(6000, 30);
-    // brake vs coast
-    left.setNeutralMode(NeutralMode.Brake);
+    left.config_kF(0, 0.046, 30);
+    left.config_kP(0, 0.05, 30);
+    left.config_kI(0, 0.001, 30);
+    left.config_kD(0, 3, 30);
 
     motorStartUp(right);
-    right.config_kF(0, 0, 30);
-    right.config_kP(0, 1, 30);
-    right.config_kI(0, 0.005, 30);
-    right.config_kD(0, 10, 30);
-    right.configMotionCruiseVelocity(20000, 30);
-    right.configMotionAcceleration(6000, 30);
-    right.setNeutralMode(NeutralMode.Brake);
+    right.config_kF(0, 0.046, 30);
+    right.config_kP(0, 0.05, 30);
+    right.config_kI(0, 0.001, 30);
+    right.config_kD(0, 3, 30);
     right.setInverted(true);
 
     motorStartUp(belt);
-    belt.config_kF(0, 0, 30); 
-    belt.config_kP(0, 1, 30);
-    belt.config_kI(0, 0.005, 30);
-    belt.config_kD(0, 10, 30);
+    intakeInternal.config_kF(0, 0, 30);
+    intakeInternal.config_kP(0, 1, 30);
+    intakeInternal.config_kI(0, 0.005, 30);
+    intakeInternal.config_kD(0, 10, 30);
     belt.configMotionCruiseVelocity(20000, 30);
     belt.configMotionAcceleration(6000, 30);
     belt.setNeutralMode(NeutralMode.Brake); 
@@ -220,7 +197,7 @@ public class Robot extends TimedRobot {
   public void motorStartUp(WPI_TalonFX motor) {
     motor.configFactoryDefault();
     motor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0 , 30);
-    motor.configNeutralDeadband(0.01, 30);
+    motor.configNeutralDeadband(0.001, 30);
     motor.setSensorPhase(false);
     motor.setInverted(false);
     motor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
@@ -230,7 +207,7 @@ public class Robot extends TimedRobot {
     motor.configPeakOutputForward(1, 30);
     motor.configPeakOutputReverse(-1, 30);
     motor.selectProfileSlot(0, 0);
-    motor.configOpenloopRamp(1);
+    motor.configOpenloopRamp(1.5);
     motor.setSelectedSensorPosition(0, 0, 30);
   }
   
@@ -249,7 +226,12 @@ public class Robot extends TimedRobot {
     positionInternalIntake = intakeInternal.getSelectedSensorPosition(0);
     positionExternalIntake = intakeExternal.getSelectedSensorPosition(0);
     time = timer.get();
-    angle = gyro.getGyroAngleZ();
+    angle = -gyro.getGyroAngleZ();
+
+    odometry.update(new Rotation2d(angle*Math.PI/180), positionLeft, positionRight);
+    robotPosition = odometry.getPoseMeters();
+    robotX = robotPosition.getX();
+    robotY = robotPosition.getY();
     
     // publishes updated variables to the dashboard
     SmartDashboard.putNumber("leftStickY", leftStickY);
@@ -265,5 +247,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Encoder (Internal Intake)", positionInternalIntake);
     SmartDashboard.putNumber("Clock",  time);
     SmartDashboard.putNumber("Angle", angle);
+    SmartDashboard.putNumber("RobotX",  robotX);
+    SmartDashboard.putNumber("RobotY", robotY);
   }
 }
