cad65f1775dca002a323d4f784b2ad0b28af5b5f
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2023-02-05 11:58:00 -0800

    Initial Ramsete Test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,18 +1,46 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
+<<<<<<< Updated upstream
+=======
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.constraint.MaxVelocityConstraint;
+>>>>>>> Stashed changes
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+<<<<<<< Updated upstream
+=======
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+>>>>>>> Stashed changes
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class Robot extends TimedRobot {
+<<<<<<< Updated upstream
   private final WPI_TalonFX left = new WPI_TalonFX(2); // left drive motor
   private final WPI_TalonFX right = new WPI_TalonFX(0); // right drive motor
   private final WPI_TalonFX belt = new WPI_TalonFX(1); // belt motor
@@ -22,6 +50,22 @@ public class Robot extends TimedRobot {
   private final XboxController controller = new XboxController(0);
   private final Timer timer = new Timer(); 
   private final ADIS16448_IMU gyro = new ADIS16448_IMU(); // RoboRIO-mounted gyroscope
+=======
+  WPI_TalonFX left = new WPI_TalonFX(2); // left drive motor
+  WPI_TalonFX right = new WPI_TalonFX(0); // right drive motor
+  WPI_TalonFX belt = new WPI_TalonFX(1); // belt motor
+  WPI_TalonFX intakeInternal = new WPI_TalonFX(3); // internal intake motor
+  WPI_TalonFX intakeExternal = new WPI_TalonFX(4); // external intake motor
+  DifferentialDrive drive = new DifferentialDrive(left, right);
+  XboxController controller = new XboxController(0);
+  Timer timer = new Timer(); 
+  ADIS16448_IMU gyro = new ADIS16448_IMU(); // RoboRIO-mounted gyroscope
+  DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(new Rotation2d(0), 0,0);
+  ProfiledPIDController angleControl = new ProfiledPIDController(0.04, 0.1, 0.01, new TrapezoidProfile.Constraints(100,20));
+  double encoderTicksPerMeter = 2048*10.71/(0.0254*6*Math.PI);
+  double desiredRobotPosition = 0;
+  double desiredRobotAngle = 0;
+>>>>>>> Stashed changes
   // controller inputs
   double leftStickY;
   double leftStickX;
@@ -40,9 +84,47 @@ public class Robot extends TimedRobot {
   // gyro angle
   double angle;
 
+  double kS = 0.13676;
+  double kV = 2.4254;
+  double kA = 0.45642;
+  double kP = 3.3778;
+  SimpleMotorFeedforward feedForward = new SimpleMotorFeedforward(kS, kV, kA);
+  
+  String trajectoryJSON = "paths/Section 1.wpilib.json";
+  Trajectory trajectory = new Trajectory();
+
+  double trackWidth = 0.69;
+  DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(trackWidth);
+  double maxV = 3;
+  double maxA = 1;
+  double ramseteB = 2;
+  double ramseteZeta = 0.7;
+  RamseteController ramsete = new RamseteController(ramseteB, ramseteZeta);
+  
+
+
   @Override
   public void robotInit() {
+<<<<<<< Updated upstream
     initialize();
+=======
+    initializeMotors();
+    timer.start();
+    timer.reset(); // sets the timer to 0
+    gyro.calibrate(); // sets the gyro angle to 0 based on the current robot position
+    CameraServer.startAutomaticCapture(); // starts the webcam stream
+    updateVariables();
+    angleControl.setIntegratorRange(1, -1);
+    angleControl.setTolerance(1);
+    angleControl.enableContinuousInput(0, 360);
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+   } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+   }
+
+>>>>>>> Stashed changes
   }
 
   @Override
@@ -57,6 +139,15 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     updateVariables();
+    Trajectory.State goal = trajectory.sample(time);
+    Pose2d robotPosition = odometry.getPoseMeters();
+    ChassisSpeeds chassisSpeeds = ramsete.calculate(robotPosition, goal);
+    DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
+    double leftSpeed = (wheelSpeeds.leftMetersPerSecond/10)*encoderTicksPerMeter;
+    double rightSpeed = (wheelSpeeds.rightMetersPerSecond/10)*encoderTicksPerMeter;
+
+    left.set(ControlMode.Velocity, leftSpeed);
+    right.set(ControlMode.Velocity, rightSpeed);
   }
 
   @Override
@@ -68,8 +159,13 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     updateVariables();
+<<<<<<< Updated upstream
     
     // sets motor speeds based on controller inputs
+=======
+
+
+>>>>>>> Stashed changes
     drive.arcadeDrive(-leftStickY, -leftStickX, true);
     belt.set(-rightTrigger);
     intakeExternal.set(leftTrigger);
@@ -179,8 +275,8 @@ public class Robot extends TimedRobot {
     rightStickX = controller.getRightX();
     leftTrigger = controller.getLeftTriggerAxis();
     rightTrigger = controller.getRightTriggerAxis();
-    positionLeft = left.getSelectedSensorPosition(0);
-    positionRight = right.getSelectedSensorPosition(0);
+    positionLeft = left.getSelectedSensorPosition(0)/encoderTicksPerMeter;
+    positionRight = right.getSelectedSensorPosition(0)/encoderTicksPerMeter;
     positionBelt = belt.getSelectedSensorPosition(0);
     positionInternalIntake = intakeInternal.getSelectedSensorPosition(0);
     positionExternalIntake = intakeExternal.getSelectedSensorPosition(0);
