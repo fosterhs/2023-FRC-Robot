c249af50658c51b68408991c60dd84b2ba2d1384
TheBigMeme <s0107592@tukwila.wednet.edu>
2023-02-23 15:53:19 -0800

    Fixed PID bug

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -74,7 +74,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     updateVariables();
-    drive.arcadeDrive(speedController.calculate(positionAverage / encoderTicksPerMeter, 1), 0);
+    drive.arcadeDrive(speedController.calculate((positionLeft + positionRight) / 2 / encoderTicksPerMeter, 1), 0);
   }
 
   @Override
